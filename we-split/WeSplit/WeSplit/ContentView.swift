ea0488a1ba950b7c53a70579a54a15416295a011
Maic Lopez Saenz <maic@lopsae.com>
2022-07-19 16:51:36 -0700

    add raw xcode project

---
+++ we-split/WeSplit/WeSplit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Maic Lopez Saenz on 7/19/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
