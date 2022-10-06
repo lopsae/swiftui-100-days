e04ae2abd1b07490fadbf6ba283cad9e7cac2971
Maic Lopez Saenz <maic@lopsae.com>
2022-10-05 23:10:21 -0700

    implement textfield with currency formatter

--- we-split/WeSplit/ContentView.swift
+++ we-split/WeSplit/ContentView.swift
@@ -5,39 +5,27 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var name = ""
-    @State private var tapCount = 0
+    @State private var checkAmount = 0.0
+    @State private var peopleCount = 2
+    @State private var tip = 20
 
-    let options = ["Uno", "Dos", "Tres"]
-    @State private var selectedOption = "Dos"
+    let tipOptions = [25, 20, 15, 10, 0]
 
     var body: some View {
         NavigationView {
             Form {
                 Section {
-                    TextField("Enter your name", text: $name)
-                    Text("Name: \(name)")
-                }
-                Section {
-                    Button("Increase tap: \(tapCount)") {
-                        tapCount += 1
-                    }
-                }
-                Section {
-                    Picker("Select your option", selection: $selectedOption) {
-                        ForEach(options, id: \.self) {
-                            Text($0)
-                        }
-                    }
-                }
-                Section {
-                    ForEach(0 ..< 7) {
-                        Text("Row No. \($0)")
-                    }
+                    TextField(
+                        "Check Amount",
+                        value: $checkAmount,
+                        format: .currency(code: Locale.current.currencyCode ?? "USD"))
+                    .keyboardType(.decimalPad)
+                    Text("Amount: \(checkAmount)")
+                    Text(checkAmount, format: .currency(code: Locale.current.currencyCode ?? "USD"))
                 }
             }
             .navigationTitle("WeSplit")
-            .navigationBarTitleDisplayMode(.inline)
+            .navigationBarTitleDisplayMode(.large)
         }
     }
 }
