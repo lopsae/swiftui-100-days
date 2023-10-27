aeb2ccc6282b9e4fa1d82485df0d2733207e2ad9
Maic Lopez Saenz <maic@lopsae.com>
2023-10-27 12:38:12 -0700

    add default xcode project

---
+++ we-split-again/WeSplitAgain/WeSplitAgain/WeSplitAgainApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitAgainApp.swift
+//  WeSplitAgain
+//
+//  Created by Maic Lopez Saenz on 10/27/23.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitAgainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
