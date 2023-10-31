3481af95e4ae6bb104ad7df622a88ffb8f625194
Maic Lopez Saenz <maic@lopsae.com>
2023-10-31 15:04:32 -0700

    play around with stacks, buttons, alerts

--- guess-the-flag/GuessTheFlag/ContentView.swift
+++ guess-the-flag/GuessTheFlag/ContentView.swift
@@ -8,14 +8,54 @@
 import SwiftUI
 
 struct ContentView: View {
+
+    @State private var showingAlert = false
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        ZStack {
+//            VStack(spacing: 0) {
+//                Color.red
+//                Color.blue
+//            }
+//            LinearGradient(
+//                stops: [
+//                    .init(color: .white, location: 0.4),
+//                    .init(color: .red, location: 0.5)],
+//                startPoint: .top, endPoint: .trailing)
+//            RadialGradient(
+//                colors: [.white, .red],
+//                center: .top, startRadius: 20, endRadius: 200)
+            AngularGradient(colors: [.white, .red, .white], center: .center)
+
+            VStack {
+                Text("With some color")
+                    .padding(50)
+//                    .foregroundStyle(.secondary)
+//                    .background(.ultraThinMaterial)
+                    .foregroundStyle(.white)
+                    .background(.red.gradient)
+                Button {
+                    showingAlert = true
+                } label: {
+                    Label("Show Alert", systemImage: "pencil")
+//                    Image(systemName: "pencil")
+//                    Text("Tap me!")
+                }
+                .alert("Important Message", isPresented: $showingAlert) {
+                    Button("OK") {}
+                    Button("One") {}
+                    Button("Two") {}
+                    Button("CNCL", role: .cancel) {}
+                    Button("Many", role: .destructive) {}
+                } message: {
+                    Text("This is the alert message.")
+                }
+//                .buttonStyle(.borderedProminent)
+//                    .tint(.mint)
+            }
+
+        }.ignoresSafeArea()
+
     }
 }
 
