f5f101bee1526a205e90091fb49539964b54fd61
Maic Lopez Saenz <maic@lopsae.com>
2023-10-31 13:26:06 -0700

    add default xcode project

---
+++ guess-the-flag/GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Maic Lopez Saenz on 10/31/23.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
