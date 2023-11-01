b257603b3b2cae5d2383b0606388a6fb8605bf10
Maic Lopez Saenz <maic@lopsae.com>
2023-11-01 13:45:34 -0700

    add pristine xcode project

---
+++ views-and-modifiers/ViewsAndModifiers/ViewsAndModifiers/ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiersApp.swift
+//  ViewsAndModifiers
+//
+//  Created by Maic Lopez Saenz on 11/1/23.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
