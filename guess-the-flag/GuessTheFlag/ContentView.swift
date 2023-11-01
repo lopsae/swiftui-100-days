70e358106955c119be9ab1541b02a5be83f4256a
Maic Lopez Saenz <maic@lopsae.com>
2023-11-01 12:55:06 -0700

    add more styling

--- guess-the-flag/GuessTheFlag/ContentView.swift
+++ guess-the-flag/GuessTheFlag/ContentView.swift
@@ -18,30 +18,47 @@ struct ContentView: View {
 
     var body: some View {
         ZStack {
-//            Color.blue.ignoresSafeArea()
-            LinearGradient(
-                colors: [.blue, .black],
-                startPoint: .top, endPoint: .bottom)
+            RadialGradient(
+                stops: [
+                    .init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.3),
+                    .init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.3)
+                ],
+                center: .top, startRadius: 200, endRadius: 400)
                 .ignoresSafeArea()
-            VStack(spacing: 30) {
-                VStack {
-                    Text("Tap the flag of")
-                        .foregroundStyle(.white)
-                        .font(.subheadline)
-                    Text(countries[correctAnswer])
-                        .foregroundStyle(.white)
-                        .font(.largeTitle.weight(.semibold))
-                }
-                ForEach(0...2, id: \.self) { index in
-                    Button {
-                        flagTapped(index: index)
-                    } label: {
-                        Image(countries[index])
-                            .clipShape(.buttonBorder)
-                            .shadow(radius: 5)
+            VStack {
+                Text("Guess the Flag")
+                    .font(.largeTitle)
+                    .foregroundColor(.white)
+                Spacer()
+                VStack(spacing: 15) {
+                    VStack {
+                        Text("Tap the flag of")
+                            .foregroundStyle(.secondary)
+                            .font(.subheadline.weight(.medium))
+                        Text(countries[correctAnswer])
+                            .font(.largeTitle.bold())
                     }
-                }
-            }
+                    ForEach(0...2, id: \.self) { index in
+                        Button {
+                            flagTapped(index: index)
+                        } label: {
+                            Image(countries[index])
+                                .clipShape(.buttonBorder)
+                                .shadow(radius: 5)
+                        }
+                    }
+                } // VStack
+                .frame(maxWidth: .infinity)
+                .padding(.vertical, 20)
+                .background(.regularMaterial)
+                .clipShape(.rect(cornerRadius: 20))
+                Spacer()
+                Text("Score: TODO")
+                    .foregroundStyle(.white)
+                    .font(.title2)
+                Spacer()
+            } // VStack
+            .padding()
         } // ZStack
         .alert(scoreTitle, isPresented: $showingScore) {
             Button("Try again") {}
