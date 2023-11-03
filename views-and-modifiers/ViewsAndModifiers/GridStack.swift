442fce925a7f8d684a13b03bbe5518d74ad15e46
Maic Lopez Saenz <maic@lopsae.com>
2023-11-03 16:44:54 -0700

    rename ambiguous var

--- views-and-modifiers/ViewsAndModifiers/GridStack.swift
+++ views-and-modifiers/ViewsAndModifiers/GridStack.swift
@@ -10,14 +10,14 @@ struct GridStack<Content: View>: View {
 
     let rows: Int
     let columns: Int
-    @ViewBuilder let content: (Int, Int) -> Content // (row, column)
+    @ViewBuilder let makeItem: (Int, Int) -> Content // (row, column)
 
     var body: some View {
         VStack {
             ForEach(0..<rows, id: \.self) { rowIndex in
                 HStack {
                     ForEach(0..<columns, id: \.self) { columnIndex in
-                        content(rowIndex, columnIndex)
+                        makeItem(rowIndex, columnIndex)
                     }
                 } // HStack
             }
