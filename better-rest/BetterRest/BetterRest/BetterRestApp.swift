c90389df8ad2d2aa9c7089b74b24055462e258d6
Maic Lopez Saenz <maic@lopsae.com>
2023-11-06 11:12:00 -0800

    add pristine XCode project

---
+++ better-rest/BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Maic Lopez Saenz on 11/6/23.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ better-rest/BetterRest/BetterRest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BetterRest
+//
+//  Created by Maic Lopez Saenz on 11/6/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Better Rest")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
