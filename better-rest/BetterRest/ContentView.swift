a3d2d3955578993327dde194308c2aeaf5bf3873
Maic Lopez Saenz <maic@lopsae.com>
2023-11-06 11:49:54 -0800

    add some date controls

--- better-rest/BetterRest/ContentView.swift
+++ better-rest/BetterRest/ContentView.swift
@@ -10,10 +10,24 @@ import SwiftUI
 struct ContentView: View {
 
     @State private var sleepAmount = 8.0
+    @State private var wakeUp: Date = {
+        var components = DateComponents()
+        components.hour = 8
+        components.minute = 0
+        return Calendar.current.date(from: components) ?? .now
+    }()
 
     var body: some View {
         Form {
-            Stepper("\(sleepAmount.formatted()) Hours", value: $sleepAmount, in: 4...12, step: 0.25)
+            Section {
+                Stepper("\(sleepAmount.formatted()) Hours", value: $sleepAmount, in: 4...12, step: 0.25)
+            }
+            Section {
+                Text("Date: \(wakeUp.formatted(date: .long, time: .shortened))")
+                DatePicker("Wakeup time", selection: $wakeUp, /*in: Date.now...,*/ displayedComponents: .hourAndMinute)
+                    .labelsHidden()
+            }
+
         }
         VStack {
             Image(systemName: "globe")
