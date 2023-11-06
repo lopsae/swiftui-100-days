c5aab97db8a0a9a4db9beefbca222e96602d3b95
Maic Lopez Saenz <maic@lopsae.com>
2023-11-06 11:18:19 -0800

    add a Stepper

--- better-rest/BetterRest/ContentView.swift
+++ better-rest/BetterRest/ContentView.swift
@@ -8,7 +8,13 @@
 import SwiftUI
 
 struct ContentView: View {
+
+    @State private var sleepAmount = 8.0
+
     var body: some View {
+        Form {
+            Stepper("\(sleepAmount.formatted()) Hours", value: $sleepAmount, in: 4...12, step: 0.25)
+        }
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
