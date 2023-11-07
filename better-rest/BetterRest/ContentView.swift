2978546e3bc001456a34f06065c8253a68e9e369
Maic Lopez Saenz <maic@lopsae.com>
2023-11-07 14:22:47 -0800

    use section titles instead of texts

--- better-rest/BetterRest/ContentView.swift
+++ better-rest/BetterRest/ContentView.swift
@@ -35,28 +35,26 @@ struct ContentView: View {
 
         return NavigationStack {
             Form {
-                Section {
-                    VStack(alignment: .leading, spacing: 0) {
-                        Text("When do you want to wake up?")
-                            .font(.headline)
+                Section("When do you want to wake up?") {
+                    VStack(alignment: .leading, spacing: 10) {
                         DatePicker("Enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
                             .labelsHidden()
                             .frame(maxWidth: .infinity)
                         Text("Raw components: h:\(wakeUpComponents.hour) m:\(wakeUpComponents.minute)")
                     }
+                }
 
+                Section("Desired amount of sleep") {
                     VStack(alignment: .leading, spacing: 0) {
-                        Text("Desired amount of sleep")
-                            .font(.headline)
                         Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
                     }
+                }
 
+                Section("Daily coffee intake") {
                     VStack(alignment: .leading, spacing: 0) {
-                        Text("Daily coffee intake")
-                            .font(.headline)
                         Stepper("^[\(coffeeAmount) cup](inflect:true)", value: $coffeeAmount, in: 1...20)
                     }
-                } // Section
+                }
 
                 Text("Sleep Time: \(bedTimeEstimation)")
 
