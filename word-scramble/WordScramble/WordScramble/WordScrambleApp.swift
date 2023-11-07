73be40c7782edc003588f66be9bb38e81923af4f
Maic Lopez Saenz <maic@lopsae.com>
2023-11-07 15:43:41 -0800

    add pristine XCode project

---
+++ word-scramble/WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Maic Lopez Saenz on 11/7/23.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
