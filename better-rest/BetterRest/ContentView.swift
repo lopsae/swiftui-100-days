ca48c9e57884ba676ad0389cf8bba6fe205e6520
Maic Lopez Saenz <maic@lopsae.com>
2023-11-07 10:55:38 -0800

    add ui from lesson

--- better-rest/BetterRest/ContentView.swift
+++ better-rest/BetterRest/ContentView.swift
@@ -10,14 +10,31 @@ import SwiftUI
 struct ContentView: View {
 
     @State private var sleepAmount = 8.0
-    @State private var wakeUp: Date = {
-        var components = DateComponents()
-        components.hour = 8
-        components.minute = 0
-        return Calendar.current.date(from: components) ?? .now
-    }()
+    @State private var wakeUp = Date.now
+    @State private var coffeeAmount = 1
 
     var body: some View {
+        NavigationStack {
+            VStack {
+                Text("When do you want to wake up?")
+                    .font(.headline)
+                DatePicker("Enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                    .labelsHidden()
+                
+                Text("Desired amount of sleep")
+                    .font(.headline)
+                Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+
+                Text("Daily coffee intake")
+                    .font(.headline)
+                Stepper("\(coffeeAmount) cup(s)", value: $coffeeAmount, in: 1...20)
+
+            } // VStack
+            .navigationTitle("Better Rest")
+            .toolbar {
+                Button("Calculate", action: calculateBedTime)
+            }
+        } // NavigationStack
         Form {
             Section {
                 Stepper("\(sleepAmount.formatted()) Hours", value: $sleepAmount, in: 4...12, step: 0.25)
@@ -27,15 +44,12 @@ struct ContentView: View {
                 DatePicker("Wakeup time", selection: $wakeUp, /*in: Date.now...,*/ displayedComponents: .hourAndMinute)
                     .labelsHidden()
             }
+        }
+    } // body
 
-        }
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Better Rest")
-        }
-        .padding()
+
+    func calculateBedTime() {
+        // TODO
     }
 }
 
