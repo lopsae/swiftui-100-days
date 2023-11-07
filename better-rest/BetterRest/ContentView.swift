a22c5ce62f6a6e0ad9fb88ea2bf721a883e0746c
Maic Lopez Saenz <maic@lopsae.com>
2023-11-07 13:48:04 -0800

    improve ui

--- better-rest/BetterRest/ContentView.swift
+++ better-rest/BetterRest/ContentView.swift
@@ -10,8 +10,15 @@ import SwiftUI
 
 struct ContentView: View {
 
+    static var defaultWakeUp: Date {
+        var components = DateComponents()
+        components.hour = 7
+        components.minute = 0
+        return Calendar.current.date(from: components) ?? .now
+    }
+    @State private var wakeUp = defaultWakeUp
+
     @State private var sleepAmount = 8.0
-    @State private var wakeUp = Date.now
     @State private var coffeeAmount = 1
 
     @State private var alertTitle = ""
@@ -20,20 +27,27 @@ struct ContentView: View {
 
     var body: some View {
         NavigationStack {
-            VStack {
-                Text("When do you want to wake up?")
-                    .font(.headline)
-                DatePicker("Enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
-                    .labelsHidden()
-                Text("Raw components: h:\(wakeUpComponents.hour) m:\(wakeUpComponents.minute)")
+            Form {
+                VStack(alignment: .leading, spacing: 0) {
+                    Text("When do you want to wake up?")
+                        .font(.headline)
+                    DatePicker("Enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                        .labelsHidden()
+                        .frame(maxWidth: .infinity)
+                    Text("Raw components: h:\(wakeUpComponents.hour) m:\(wakeUpComponents.minute)")
+                }
 
-                Text("Desired amount of sleep")
-                    .font(.headline)
-                Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+                VStack(alignment: .leading, spacing: 0) {
+                    Text("Desired amount of sleep")
+                        .font(.headline)
+                    Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+                }
 
-                Text("Daily coffee intake")
-                    .font(.headline)
-                Stepper("\(coffeeAmount) cup(s)", value: $coffeeAmount, in: 1...20)
+                VStack(alignment: .leading, spacing: 0) {
+                    Text("Daily coffee intake")
+                        .font(.headline)
+                    Stepper("^[\(coffeeAmount) cup](inflect:true)", value: $coffeeAmount, in: 1...20)
+                }
 
             } // VStack
             .navigationTitle("Better Rest")
