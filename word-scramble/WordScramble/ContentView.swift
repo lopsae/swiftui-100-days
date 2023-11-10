fff427c5e2fcd56b9c64c2e9c8bd67cacd6f7377
Maic Lopez Saenz <maic@lopsae.com>
2023-11-09 17:01:35 -0800

    add a List

--- word-scramble/WordScramble/ContentView.swift
+++ word-scramble/WordScramble/ContentView.swift
@@ -7,14 +7,39 @@ import SwiftUI
 
 
 struct ContentView: View {
+
+    private let names = ["Pedro", "Pablo", "Jose", "Jose"] 
+    // jose repeats, which produces a runtime warning
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("WordScramble!")
-        }
-        .padding()
+        List {
+            Section("Text section") {
+                Text("One text")
+                Text("Two text")
+                Text("Three text")
+            }
+
+            Section("Dynamic section") {
+                ForEach(0..<5) {
+                    Text("Dynamic Text \($0)")
+                }
+            }
+
+            Section("Names section") {
+                ForEach(names, id: \.self) {
+                    Text("Name: \($0)")
+                }
+            }
+
+            Section("VStack Section") {
+                VStack(alignment: .center) {
+                    Image(systemName: "globe")
+                        .imageScale(.large)
+                        .foregroundStyle(.tint)
+                    Text("WordScramble!")
+                }.frame(maxWidth: .infinity)
+            }
+        }.listStyle(.grouped)
     }
 }
 
