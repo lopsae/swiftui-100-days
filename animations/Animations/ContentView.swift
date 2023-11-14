535ff4a7073dba63c2674617896f309b9364fa2c
Maic Lopez Saenz <maic@lopsae.com>
2023-11-14 12:15:51 -0800

    animate a button to grow and blur

--- animations/Animations/ContentView.swift
+++ animations/Animations/ContentView.swift
@@ -2,10 +2,25 @@
 // Animations
 //
 
+
 import SwiftUI
 
+
 struct ContentView: View {
+
+    @State private var animationFactor = 1.0
+
+
     var body: some View {
+        Button("Tap me") {
+            animationFactor += 0.5
+        }.padding(50)
+            .background(.red.gradient)
+            .foregroundStyle(.white)
+            .clipShape(.circle)
+            .scaleEffect(animationFactor)
+            .blur(radius: (animationFactor - 1) * 3)
+            .animation(.default, value: animationFactor)
         VStack {
             Image(systemName: "paintbrush.pointed")
                 .imageScale(.large)
