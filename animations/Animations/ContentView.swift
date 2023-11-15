17c394289bbe37151d1d1a1865c028e42d0d5d85
Maic Lopez Saenz <maic@lopsae.com>
2023-11-15 11:40:37 -0800

    add stepper with animated change

--- animations/Animations/ContentView.swift
+++ animations/Animations/ContentView.swift
@@ -8,35 +8,26 @@ import SwiftUI
 
 struct ContentView: View {
 
-    @State private var animationFactor = 1.0
+    @State private var redButtonFactor = 1.0
 
 
     var body: some View {
-        Button("Tap me") {
-//            animationFactor += 1.0
-        }.padding(50)
-            .background(.red.gradient)
-            .foregroundStyle(.white)
-            .clipShape(.circle)
-//            .scaleEffect(animationFactor)
-            .overlay {
-                Circle()
-                    .stroke(.red)
-                    .scaleEffect(animationFactor)
-                    .opacity(2-animationFactor)
-                    .animation(.easeOut(duration: 1).repeatForever(autoreverses: false), value: animationFactor)
-            }
-            .onAppear {
-                animationFactor = 2
-            }
-//            .animation(.easeInOut(duration: 1).repeatCount(3, autoreverses: true), value: animationFactor)
-        VStack {
-            Image(systemName: "paintbrush.pointed")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Animations")
-        }
-        .padding()
+        print("🍊 redButtonFactor: \(redButtonFactor)")
+        return List {
+            Stepper("Scale amount", value: $redButtonFactor.animation(.bouncy(duration: 2)), in: 1...10)
+            // Text also animates when change comes from the stepper
+            Text("scaleFactor: \(redButtonFactor)")
+            Button("Scale +1") {
+                redButtonFactor += 1.0
+            }.padding(50)
+                .background(.red.gradient)
+                .foregroundStyle(.white)
+                .clipShape(.circle)
+            // How is this knowing that it should animate?
+            // Is $.animation() creating an animation transaction everytime it changes?
+                .scaleEffect(redButtonFactor)
+                .frame(maxWidth: .infinity)
+        } // VStack
     }
 }
 
