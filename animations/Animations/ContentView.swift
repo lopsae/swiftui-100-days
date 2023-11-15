b6691169973c92a206b9b3e9d27a441a05e52078
Maic Lopez Saenz <maic@lopsae.com>
2023-11-15 12:00:32 -0800

    add a rotation animated button

--- animations/Animations/ContentView.swift
+++ animations/Animations/ContentView.swift
@@ -9,24 +9,40 @@ import SwiftUI
 struct ContentView: View {
 
     @State private var redButtonFactor = 1.0
+    @State private var rotationAmount = Double.zero
 
 
     var body: some View {
         print("🍊 redButtonFactor: \(redButtonFactor)")
         return List {
-            Stepper("Scale amount", value: $redButtonFactor.animation(.bouncy(duration: 2)), in: 1...10)
-            // Text also animates when change comes from the stepper
-            Text("scaleFactor: \(redButtonFactor)")
-            Button("Scale +1") {
-                redButtonFactor += 1.0
-            }.padding(50)
-                .background(.red.gradient)
-                .foregroundStyle(.white)
-                .clipShape(.circle)
-            // How is this knowing that it should animate?
-            // Is $.animation() creating an animation transaction everytime it changes?
-                .scaleEffect(redButtonFactor)
-                .frame(maxWidth: .infinity)
+            Section {
+                Button("Rotate me") {
+                    withAnimation(.spring(duration: 2, bounce: 0.5)) {
+                        rotationAmount += 70
+                    }
+                }
+                    .padding(50)
+                    .background(.blue.gradient)
+                    .foregroundStyle(.white)
+                    .clipShape(.circle)
+                    .frame(maxWidth: .infinity)
+                    .rotation3DEffect(.degrees(rotationAmount), axis: (x: 0, y: 1, z: 0))
+            }
+            Section("Scale animation") {
+                Stepper("Scale amount", value: $redButtonFactor.animation(.bouncy(duration: 2)), in: 1...10)
+                // Text also animates when change comes from the stepper
+                Text("scaleFactor: \(redButtonFactor)")
+                Button("Scale +1") {
+                    redButtonFactor += 1.0
+                }.padding(50)
+                    .background(.red.gradient)
+                    .foregroundStyle(.white)
+                    .clipShape(.circle)
+                // How is this knowing that it should animate?
+                // Is $.animation() creating an animation transaction everytime it changes?
+                    .scaleEffect(redButtonFactor)
+                    .frame(maxWidth: .infinity)
+            }
         } // VStack
     }
 }
