e3d3581c02be0852b89e640b9ba946f6ab6cfcf4
Maic Lopez Saenz <maic@lopsae.com>
2023-11-28 13:46:32 -0800

    add examples for transitions

--- animations/Animations/TransitionExamples.swift
+++ animations/Animations/TransitionExamples.swift
@@ -8,9 +8,31 @@ import SwiftUI
 
 struct TransitionExamples: View {
 
+    @State private var isShowingRed = false
+
     var body: some View {
-        Text("TransitionExamples")
-    }
+        VStack {
+            Button("Bouncy animate") {
+                withAnimation(.bouncy) {
+                    isShowingRed.toggle()
+                }
+            }.buttonStyle(.bordered)
+
+            Button("Default animate") {
+                withAnimation {
+                    isShowingRed.toggle()
+                }
+            }.buttonStyle(.bordered)
+
+            if isShowingRed {
+                Rectangle()
+                    .fill(.red)
+                    .frame(width: 200, height: 200)
+                    .transition(.asymmetric(insertion: .scale, removal: .opacity))
+            }
+
+        } // VStack
+    } // Body
 
 }
 
