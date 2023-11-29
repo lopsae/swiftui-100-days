f1ca628d2c5efde7b4644db32938f222526f0e22
Maic Lopez Saenz <maic@lopsae.com>
2023-11-29 12:46:12 -0800

    playing with Observable objects

--- iexpense/iExpense/ContentView.swift
+++ iexpense/iExpense/ContentView.swift
@@ -1,20 +1,42 @@
 //
 // iExpense
 //
-//
 
 import SwiftUI
+import Observation
 
 struct ContentView: View {
+
+    @State private var user = User()
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("iExpense")
-        }
+            Text("Your name is \(user.firstName) \(user.lastName).")
+            TextField("First name", text: $user.firstName)
+            TextField("Last name", text: $user.lastName)
+        } // VStack
         .padding()
+        .onAppear {
+            let oneUser = User()
+            let otherUser = oneUser
+            otherUser.lastName = "Malachite"
+
+            print("One: \(oneUser)")
+            print("Other: \(otherUser)")
+        }
     }
+}
+
+
+@Observable class User: CustomStringConvertible {
+
+    var firstName: String = "Black"
+    var lastName: String = "Quartz"
+
+    var description: String {
+        "User firstName:\(firstName) lastName:\(lastName)"
+    }
+
 }
 
 #Preview {
