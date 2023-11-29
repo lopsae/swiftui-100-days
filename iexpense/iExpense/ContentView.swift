b121bf1e6c0187063365d0aaca58933f7bc93098
Maic Lopez Saenz <maic@lopsae.com>
2023-11-29 13:21:06 -0800

    add list with remove capability

--- iexpense/iExpense/ContentView.swift
+++ iexpense/iExpense/ContentView.swift
@@ -10,27 +10,57 @@ struct ContentView: View {
     @State private var user = User()
     @State private var isSheetVisible = false
 
-    var body: some View {
-        VStack {
-            Text("Your name is \(user.firstName) \(user.lastName).")
-            TextField("First name", text: $user.firstName)
-            TextField("Last name", text: $user.lastName)
-            Button("Show sheet") {
-                isSheetVisible.toggle()
-            }.buttonStyle(.bordered)
-        } // VStack
-        .padding()
-        .sheet(isPresented: $isSheetVisible) {
-            SecondView(name: user.firstName)
-        }
-        .onAppear {
-            let oneUser = User()
-            let otherUser = oneUser
-            otherUser.lastName = "Malachite"
+    @State private var numbers: [Int] = [1, 2, 3]
+    @State private var nextNumber = 4
 
-            print("One: \(oneUser)")
-            print("Other: \(otherUser)")
-        }
+    var body: some View {
+        NavigationStack {
+            Form {
+                Section {
+                    Text("Your name is \(user.firstName) \(user.lastName).")
+                    TextField("First name", text: $user.firstName)
+                    TextField("Last name", text: $user.lastName)
+                    Button("Show sheet") {
+                        isSheetVisible.toggle()
+                    }.buttonStyle(.bordered)
+                        .frame(maxWidth: .infinity)
+                } // Section
+
+                Section {
+                    List {
+                        ForEach(numbers, id: \.self) {
+                            Text("Row: \($0)")
+                        }
+                        .onDelete(perform: removeRows(at:))
+                    }
+                    Button("Add number") {
+                        numbers.append(nextNumber)
+                        nextNumber += 1
+                    }.buttonStyle(.bordered)
+                        .frame(maxWidth: .infinity)
+                } // Section
+            } // Form
+            .navigationTitle("Form")
+            .toolbar {
+                EditButton()
+            }
+            .sheet(isPresented: $isSheetVisible) {
+                SecondView(name: user.firstName)
+            }
+            .onAppear {
+                let oneUser = User()
+                let otherUser = oneUser
+                otherUser.lastName = "Malachite"
+
+                print("One: \(oneUser)")
+                print("Other: \(otherUser)")
+            }
+        } // NavigationStack
+    } // body
+
+
+    func removeRows(at offsets: IndexSet) {
+        numbers.remove(atOffsets: offsets)
     }
 }
 
