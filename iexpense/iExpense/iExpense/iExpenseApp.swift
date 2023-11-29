c954290ff956cccd1ea3daa54f8fd8aafeb72a3c
Maic Lopez Saenz <maic@lopsae.com>
2023-11-29 10:45:09 -0800

    add pristine XCode project

---
+++ iexpense/iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Maic Lopez Saenz on 11/29/23.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
