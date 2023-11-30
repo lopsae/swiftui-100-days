174ac0475ca129dccdc0bb20421358fcee923330
Maic Lopez Saenz <maic@lopsae.com>
2023-11-30 15:49:29 -0800

    separate Expenses into its own file

--- iexpense/iExpense/ExpenseItem.swift
+++ iexpense/iExpense/ExpenseItem.swift
@@ -4,7 +4,6 @@
 
 
 import Foundation
-import Observation
 
 
 struct ExpenseItem: Identifiable, Codable {
@@ -14,53 +13,6 @@ struct ExpenseItem: Identifiable, Codable {
     let name: String
     let type: String
     let amount: Double
-
-}
-
-
-@Observable
-class Expenses {
-
-    static let storageKey = "expenses.items"
-
-    let storage: UserDefaults?
-
-    var items: [ExpenseItem] {
-        didSet {
-            if let storage,
-               let encoded = try? JSONEncoder().encode(items)
-            {
-                storage.set(encoded, forKey: Self.storageKey)
-            }
-        }
-    }
-
-
-    init(storage: UserDefaults) {
-        self.storage = storage
-        if let storedData = storage.data(forKey: Self.storageKey),
-           let decoded = try? JSONDecoder().decode([ExpenseItem].self, from: storedData)
-        {
-            items = decoded
-        } else {
-            items = []
-        }
-    }
-
-
-    init(items: [ExpenseItem]) {
-        self.storage = nil
-        self.items = items
-    }
-
-
-    static var preview: Expenses {
-        return Expenses(items: [
-            .init(name: "Antique", type: "Personal", amount: 57.0),
-            .init(name: "Bubbles", type: "Business", amount: 12.0),
-            .init(name: "Chocolate", type: "Personal", amount: 10.0),
-        ])
-    }
 
 }
 
---
+++ iexpense/iExpense/Expenses.swift
@@ -0,0 +1,54 @@
+//
+// iExpense
+//
+
+
+import Foundation
+import Observation
+
+
+@Observable class Expenses {
+
+    static let storageKey = "expenses.items"
+
+    let storage: UserDefaults?
+
+    var items: [ExpenseItem] {
+        didSet {
+            if let storage,
+               let encoded = try? JSONEncoder().encode(items)
+            {
+                storage.set(encoded, forKey: Self.storageKey)
+            }
+        }
+    }
+
+
+    init(storage: UserDefaults) {
+        self.storage = storage
+        if let storedData = storage.data(forKey: Self.storageKey),
+           let decoded = try? JSONDecoder().decode([ExpenseItem].self, from: storedData)
+        {
+            items = decoded
+        } else {
+            items = []
+        }
+    }
+
+
+    init(items: [ExpenseItem]) {
+        self.storage = nil
+        self.items = items
+    }
+
+
+    static var preview: Expenses {
+        return Expenses(items: [
+            .init(name: "Antique", type: "Personal", amount: 57.0),
+            .init(name: "Bubbles", type: "Business", amount: 12.0),
+            .init(name: "Chocolate", type: "Personal", amount: 10.0),
+        ])
+    }
+
+}
+
