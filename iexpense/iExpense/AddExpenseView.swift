b3adc7f797b4c2e7b23c629b784fe9cbb9742fa2
Maic Lopez Saenz <maic@lopsae.com>
2023-11-29 17:54:49 -0800

    add AddExpenseView

---
+++ iexpense/iExpense/AddExpenseView.swift
@@ -0,0 +1,37 @@
+//
+// iExpense
+//
+
+
+import SwiftUI
+
+
+struct AddExpenseView: View {
+    
+    @State private var name = ""
+    @State private var type = "Personal"
+    @State private var amount = 10.0
+
+    let types = ["Business", "Personal"]
+
+    var body: some View {
+        NavigationStack {
+            Form {
+                TextField("Name", text: $name)
+                Picker("Type", selection: $type) {
+                    ForEach(types, id: \.self) {
+                        Text($0)
+                    }
+                }
+                TextField("Amount", value: $amount, format: .currency(code: "USD"))
+                    .keyboardType(.decimalPad)
+            } // Form
+            .navigationTitle("Add new expense")
+        } // NavigationStack
+    }
+}
+
+#Preview {
+    AddExpenseView()
+}
+
