438563382ded40b750a702b0b17ca829dd0a07ff
Maic Lopez Saenz <maic@lopsae.com>
2023-11-30 12:03:20 -0800

    display AddExpensesView and save new expense

--- iexpense/iExpense/AddExpenseView.swift
+++ iexpense/iExpense/AddExpenseView.swift
@@ -12,7 +12,13 @@ struct AddExpenseView: View {
     @State private var type = "Personal"
     @State private var amount = 10.0
 
+    // TODO: could be an enum
     let types = ["Business", "Personal"]
+
+    var expenses: Expenses
+
+    @Environment(\.dismiss) var dismiss
+
 
     var body: some View {
         NavigationStack {
@@ -27,11 +33,20 @@ struct AddExpenseView: View {
                     .keyboardType(.decimalPad)
             } // Form
             .navigationTitle("Add new expense")
+            .toolbar {
+                Button("Save") {
+                    let newExpense = ExpenseItem(
+                        name: name, type: type, amount: amount)
+                    expenses.items.append(newExpense)
+                    dismiss()
+                }
+            }
         } // NavigationStack
     }
 }
 
+
 #Preview {
-    AddExpenseView()
+    AddExpenseView(expenses: Expenses())
 }
 
--- iexpense/iExpense/ExpensesView.swift
+++ iexpense/iExpense/ExpensesView.swift
@@ -10,9 +10,12 @@ struct ExpensesView: View {
 
     @State private var expenses = Expenses()
 
+    @State private var isAddExpensesVisible = false
+
     var body: some View {
         NavigationStack {
             List {
+                // TODO: can a default element be added for empty lists?
                 ForEach(expenses.items) { item in
                     Text(item.name)
                 }
@@ -20,13 +23,19 @@ struct ExpensesView: View {
             } // List
             .navigationTitle("Expenses")
             .toolbar {
-                Button("Add Expense") {
+                Button("Add Test") {
                     let componentSet: Set = [Calendar.Component.hour, .minute, .second, .nanosecond]
                     let dateComponents = Calendar.current.dateComponents(componentSet, from: .now)
                     let name = "Test-\(dateComponents.hour!)\(dateComponents.minute!)\(dateComponents.second!)\(dateComponents.nanosecond!)"
                     let newExpense = ExpenseItem(name: name, type: "Personal", amount: 10.0)
                     expenses.items.append(newExpense)
                 }
+                Button("Add Expense", systemImage: "plus") {
+                    isAddExpensesVisible = true
+                }
+            }
+            .sheet(isPresented: $isAddExpensesVisible) {
+                AddExpenseView(expenses: expenses)
             }
         } // NavigationStack
     } // body
